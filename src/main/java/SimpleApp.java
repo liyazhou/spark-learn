ea7d95acaf0a46e45ae9e5b7316520911d2c414f
liyazhou <liyazhou@outlook.com>
2017-01-23 17:36:56 +0800

    Initial commit

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,34 @@
+/**
+ * Created by liyazhou on 2017/1/22.
+ */
+/* SimpleApp.java */
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+//        String logFile = "/Users/liyazhou/workspace/spark-learn/README.md"; // Should be some file on your system
+        String logFile = "README.md"; // Should be some file on your system
+        SparkConf conf = new SparkConf().setAppName("Simple Application");
+//        conf.setMaster("spark://localhost:7077");
+        conf.setMaster("spark://192.168.112.245:7077");
+        conf.setJars(new String[]{"/Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar"});
+        JavaSparkContext sc = new JavaSparkContext(conf);
+//        sc.addJar("/Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar");
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        sc.stop();
+    }
+
+}
