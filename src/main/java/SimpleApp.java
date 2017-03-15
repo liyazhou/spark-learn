133de13bfb419eabc1309444139d2a0981e07cce
liyazhou <liyazhou@outlook.com>
2017-03-15 14:52:45 +0800

    add

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -1,34 +1,45 @@
 /**
- * Created by liyazhou on 2017/1/22.
+ * Created by liyazhou on 2017/3/15.
  */
-/* SimpleApp.java */
-import org.apache.spark.api.java.*;
+    /* SimpleApp.java */
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+//    ./bin/spark-submit --class "SimpleApp" --master spark://127.0.0.1:7077 /Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar
+//    ./bin/spark-submit --class "SimpleApp" --master spark://192.168.112.245:7077 /Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar
+//    ./bin/spark-submit --class "SimpleApp" --master spark://192.168.222.55:7077 /Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar
+//    把jar上传到master 所在的服务器路径
+//    ./bin/spark-submit --class "SimpleApp" --master spark://192.168.222.55:7077 spark-learn-1.0-SNAPSHOT.jar
 
 public class SimpleApp {
     public static void main(String[] args) {
-//        String logFile = "/Users/liyazhou/workspace/spark-learn/README.md"; // Should be some file on your system
-        String logFile = "README.md"; // Should be some file on your system
+        String logFile = "./README.md"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
-//        conf.setMaster("spark://localhost:7077");
-        conf.setMaster("spark://192.168.112.245:7077");
-        conf.setJars(new String[]{"/Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar"});
         JavaSparkContext sc = new JavaSparkContext(conf);
-//        sc.addJar("/Users/liyazhou/workspace/spark-learn/target/spark-learn-1.0-SNAPSHOT.jar");
         JavaRDD<String> logData = sc.textFile(logFile).cache();
 
         long numAs = logData.filter(new Function<String, Boolean>() {
-            public Boolean call(String s) { return s.contains("a"); }
+            public Boolean call(String s) {
+                return s.contains("a");
+            }
         }).count();
 
         long numBs = logData.filter(new Function<String, Boolean>() {
-            public Boolean call(String s) { return s.contains("b"); }
+            public Boolean call(String s) {
+                return s.contains("b");
+            }
         }).count();
 
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+        long numCs = logData.filter(new Function<String, Boolean>() {
+            @Override
+            public Boolean call(String s) throws Exception {
+                return s.contains("c");
+            }
+        }).count();
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs + ", lines with c: " + numCs);
 
         sc.stop();
     }
-
 }
