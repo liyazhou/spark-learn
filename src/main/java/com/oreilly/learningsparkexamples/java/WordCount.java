dfc82025e33b55a8842d8b0a69cb40762dea1417
liyazhou <liyazhou@outlook.com>
2017-04-12 11:00:25 +0800

    add word count

---
+++ src/main/java/com/oreilly/learningsparkexamples/java/WordCount.java
@@ -0,0 +1,41 @@
+/**
+ * Illustrates a wordcount in Java
+ */
+package com.oreilly.learningsparkexamples.java;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        args = new String[]{"local", "./README.md", "./files/result"};
+        String master = args[0];
+        JavaSparkContext sc = new JavaSparkContext(
+                master, "wordcount", System.getenv("SPARK_HOME"), System.getenv("JARS"));
+        JavaRDD<String> rdd = sc.textFile(args[1]);
+        JavaPairRDD<String, Integer> counts = rdd.flatMap(
+                new FlatMapFunction<String, String>() {
+                    public Iterator<String> call(String x) {
+                        return Arrays.asList(x.split(" ")).iterator();
+                    }
+                }).mapToPair(new PairFunction<String, String, Integer>() {
+            public Tuple2<String, Integer> call(String x) {
+                return new Tuple2(x, 1);
+            }
+        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer x, Integer y) {
+                return x + y;
+            }
+        });
+        counts.saveAsTextFile(args[2]);
+    }
+}
